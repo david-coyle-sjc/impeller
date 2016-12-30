42557dbadad94f2bbd51cec0b667c2af08f1f9f8
Drew McCormack <drewmccormack@mac.com>
2016-12-30 13:27:33 +0100

    Improved ValueTree class.
    Added deep copying, made class final, and added some useful properties to the Property enum.

--- Impeller/ValueTree.swift
+++ Impeller/ValueTree.swift
@@ -16,9 +16,58 @@ public struct ValueTreeReference: Equatable {
 }
 
 
-public class ValueTree: Equatable, Hashable {
+public final class ValueTree: Equatable, Hashable {
     public var metadata: Metadata
     public var storageType: StorageType
+    
+    private var propertiesByKey = [String:Property]()
+    
+    public var valueTreeReference: ValueTreeReference {
+        return ValueTreeReference(uniqueIdentifier: metadata.uniqueIdentifier, storageType: storageType)
+    }
+
+    public init(storageType: StorageType, metadata: Metadata) {
+        self.storageType = storageType
+        self.metadata = metadata
+    }
+    
+    public init(deepCopying other:ValueTree) {
+        metadata = other.metadata
+        storageType = other.storageType
+        
+        var newPropertiesByKey = [String:Property]()
+        for (key, value) in other.propertiesByKey {
+            switch value {
+            case .valueTree(let tree):
+                newPropertiesByKey[key] = .valueTree(ValueTree(deepCopying:tree))
+            case .valueTrees(let trees):
+                newPropertiesByKey[key] = .valueTrees(trees.map { ValueTree(deepCopying:$0) })
+            case .optionalValueTree(let tree):
+                newPropertiesByKey[key] = .optionalValueTree(tree == nil ? nil : ValueTree(deepCopying:tree!))
+            default:
+                newPropertiesByKey[key] = value
+            }
+        }
+        propertiesByKey = newPropertiesByKey
+    }
+    
+    public func get(_ key: String) -> Property? {
+        return propertiesByKey[key]
+    }
+    
+    public func set(_ key: String, to property: Property) {
+        propertiesByKey[key] = property
+    }
+    
+    public var hashValue: Int {
+        return metadata.uniqueIdentifier.hash
+    }
+    
+    public static func ==(left: ValueTree, right: ValueTree) -> Bool {
+        let l = left.propertiesByKey.mapValues { $0.referenceTransformed() }
+        let r = right.propertiesByKey.mapValues { $0.referenceTransformed() }
+        return l == r && left.metadata == right.metadata && left.storageType == right.storageType
+    }
     
     public enum Property: Equatable {
         case primitive(AnyStorablePrimitive)
@@ -30,6 +79,52 @@ public class ValueTree: Equatable, Hashable {
         case valueTreeReference(ValueTreeReference)
         case optionalValueTreeReference(ValueTreeReference?)
         case valueTreeReferences([ValueTreeReference])
+        
+        public var type: Int16 {
+            switch self {
+            case .primitive:
+                return 10
+            case .optionalPrimitive:
+                return 20
+            case .primitives:
+                return 30
+            case .valueTree:
+                return 40
+            case .optionalValueTree:
+                return 50
+            case .valueTrees:
+                return 60
+            case .valueTreeReference:
+                return 70
+            case .optionalValueTreeReference:
+                return 80
+            case .valueTreeReferences:
+                return 90
+            }
+        }
+        
+        public var isOptionalType: Bool {
+            switch self {
+            case .primitive:
+                return false
+            case .optionalPrimitive:
+                return true
+            case .primitives:
+                return false
+            case .valueTree:
+                return false
+            case .optionalValueTree:
+                return true
+            case .valueTrees:
+                return false
+            case .valueTreeReference:
+                return false
+            case .optionalValueTreeReference:
+                return true
+            case .valueTreeReferences:
+                return false
+            }
+        }
         
         public func asPrimitive() -> AnyStorablePrimitive? {
             switch self {
@@ -155,35 +250,6 @@ public class ValueTree: Equatable, Hashable {
                 return false
             }
         }
-    }
-    
-    private var propertiesByKey = [String:Property]()
-    
-    public var valueTreeReference: ValueTreeReference {
-        return ValueTreeReference(uniqueIdentifier: metadata.uniqueIdentifier, storageType: storageType)
-    }
-
-    public init(storageType: StorageType, metadata: Metadata) {
-        self.storageType = storageType
-        self.metadata = metadata
-    }
-    
-    public func get(_ key: String) -> Property? {
-        return propertiesByKey[key]
-    }
-    
-    public func set(_ key: String, to property: Property) {
-        propertiesByKey[key] = property
-    }
-    
-    public var hashValue: Int {
-        return metadata.uniqueIdentifier.hash
-    }
-    
-    public static func ==(left: ValueTree, right: ValueTree) -> Bool {
-        let l = left.propertiesByKey.mapValues { $0.referenceTransformed() }
-        let r = right.propertiesByKey.mapValues { $0.referenceTransformed() }
-        return l == r && left.metadata == right.metadata && left.storageType == right.storageType
     }
 }
 
