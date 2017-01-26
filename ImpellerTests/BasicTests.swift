f3852c49c6486d24ae4632b3f1cc44e432069e5a
Drew McCormack <drewmccormack@mac.com>
2017-01-26 13:19:12 +0100

    Added unit tests for deletions and orphans.

--- ImpellerTests/BasicTests.swift
+++ ImpellerTests/BasicTests.swift
@@ -11,10 +11,10 @@ import XCTest
 
 class BasicTests: XCTestCase {
     
-    var respository: MonolithicRepository!
+    var repository: MonolithicRepository!
     
     override func setUp() {
-        respository = MonolithicRepository()
+        repository = MonolithicRepository()
     }
     
     func testSave() {
@@ -23,7 +23,7 @@ class BasicTests: XCTestCase {
         person.age = 10
         person.tags.append("Tag")
         
-        respository.commit(&person)
+        repository.commit(&person)
         
         XCTAssertEqual(person.name, "Bob")
         XCTAssertEqual(person.age, 10)
@@ -36,9 +36,9 @@ class BasicTests: XCTestCase {
         person.age = 10
         person.tags = ["tag"]
         
-        respository.commit(&person)
+        repository.commit(&person)
         
-        let fetchedPerson:Person? = respository.fetchValue(identifiedBy: person.metadata.uniqueIdentifier)
+        let fetchedPerson:Person? = repository.fetchValue(identifiedBy: person.metadata.uniqueIdentifier)
         XCTAssertEqual(fetchedPerson!.name, "Bob")
         XCTAssertEqual(fetchedPerson!.age, 10)
         XCTAssertEqual(fetchedPerson!.tags, ["tag"])
@@ -50,11 +50,11 @@ class BasicTests: XCTestCase {
         person.age = 10
         XCTAssertEqual(person.metadata.version, 0)
 
-        respository.commit(&person)
+        repository.commit(&person)
         XCTAssertEqual(person.metadata.version, 0)
         
         person.name = "Dave"
-        respository.commit(&person)
+        repository.commit(&person)
         XCTAssertEqual(person.metadata.version, 1)
     }
     
@@ -62,10 +62,10 @@ class BasicTests: XCTestCase {
         var person = Person()
         person.name = "Bob"
         person.age = 10
-        respository.commit(&person)
+        repository.commit(&person)
         
         let metadata = person.metadata
-        respository.commit(&person)
+        repository.commit(&person)
         XCTAssertEqual(metadata, person.metadata)
     }
     
@@ -73,77 +73,77 @@ class BasicTests: XCTestCase {
         var person = Person()
         person.name = "Bob"
         person.age = nil
-        respository.commit(&person)
+        repository.commit(&person)
         
-        let fetchedPerson:Person? = respository.fetchValue(identifiedBy: person.metadata.uniqueIdentifier)
+        let fetchedPerson:Person? = repository.fetchValue(identifiedBy: person.metadata.uniqueIdentifier)
         XCTAssertNil(fetchedPerson!.age)
     }
     
     func testParentWithChild() {
         var parent = Parent()
         parent.child.age = 10
-        respository.commit(&parent)
+        repository.commit(&parent)
 
-        let fetchedChild:Child! = respository.fetchValue(identifiedBy: parent.child.metadata.uniqueIdentifier)
+        let fetchedChild:Child! = repository.fetchValue(identifiedBy: parent.child.metadata.uniqueIdentifier)
         XCTAssertEqual(fetchedChild.age, 10)
 
-        let fetchedParent:Parent! = respository.fetchValue(identifiedBy: parent.metadata.uniqueIdentifier)
+        let fetchedParent:Parent! = repository.fetchValue(identifiedBy: parent.metadata.uniqueIdentifier)
         XCTAssertEqual(fetchedParent.child.age, 10)
     }
     
-    func testChangingChildInrespositoryChangesFetchedParent() {
+    func testChangingChildInrepositoryChangesFetchedParent() {
         var parent = Parent()
         parent.child.age = 10
-        respository.commit(&parent)
+        repository.commit(&parent)
         
-        var child:Child? = respository.fetchValue(identifiedBy: parent.child.metadata.uniqueIdentifier)
+        var child:Child? = repository.fetchValue(identifiedBy: parent.child.metadata.uniqueIdentifier)
         child!.age = 20
-        respository.commit(&child!)
+        repository.commit(&child!)
         
         XCTAssertEqual(parent.child.age, 10)
         XCTAssertEqual(child!.age, 20)
 
-        let fetchedParent:Parent! = respository.fetchValue(identifiedBy: parent.metadata.uniqueIdentifier)
+        let fetchedParent:Parent! = repository.fetchValue(identifiedBy: parent.metadata.uniqueIdentifier)
         XCTAssertEqual(fetchedParent.child.age, 20)
     }
     
     func testChangingChildButSavingParent() {
         var parent = Parent()
         parent.child.age = 10
-        respository.commit(&parent)
+        repository.commit(&parent)
         XCTAssertEqual(parent.child.age, 10)
 
         parent.child.age = 20
         XCTAssertEqual(parent.child.age, 20)
 
-        respository.commit(&parent)
+        repository.commit(&parent)
         XCTAssertEqual(parent.child.age, 20)
 
-        let child:Child? = respository.fetchValue(identifiedBy: parent.child.metadata.uniqueIdentifier)
+        let child:Child? = repository.fetchValue(identifiedBy: parent.child.metadata.uniqueIdentifier)
         XCTAssertEqual(child!.age, 20)
     }
     
     func testResolvingConflicts() {
         var child = Child()
         child.age = 10
-        respository.commit(&child)
+        repository.commit(&child)
     
-        // Update and set metadata to preceed respositoryd value
+        // Update and set metadata to preceed repositoryd value
         child.age = 20
         child.metadata.timestamp -= 1.0
         child.metadata.version += 1
-        respository.commit(&child)
+        repository.commit(&child)
         
-        // Ensure the respositoryd values survive, due to having more recent timestamp
+        // Ensure the repositoryd values survive, due to having more recent timestamp
         XCTAssertEqual(child.age, 10)
         
         // Now set to later timestamp and commit
         child.age = 20
         child.metadata.timestamp += 1.0
         child.metadata.version += 1
-        respository.commit(&child)
+        repository.commit(&child)
         
-        // Ensure the respositoryd values are updated
+        // Ensure the repositoryd values are updated
         XCTAssertEqual(child.age, 20)
     }
     
@@ -152,13 +152,62 @@ class BasicTests: XCTestCase {
         var child1 = Child() ; child1.age = 10
         var child2 = Child() ; child2.age = 12
         parent.children = [child1, child2]
-        respository.commit(&parent)
+        repository.commit(&parent)
         
-        let fetchedParent:Parent? = respository.fetchValue(identifiedBy: parent.metadata.uniqueIdentifier)
+        let fetchedParent:Parent? = repository.fetchValue(identifiedBy: parent.metadata.uniqueIdentifier)
         XCTAssertEqual(fetchedParent!.children.count, 2)
         
         let fetchedChild2 = fetchedParent?.children[1]
         XCTAssertEqual(fetchedChild2!.age, 12)
+    }
+    
+    func testChangingChildrenDeletesOrphans() {
+        var parent = Parent()
+        let child1 = Child()
+        let child2 = Child()
+        parent.children = [child1, child2]
+        repository.commit(&parent)
+        
+        do {
+            let fetchedChild:Child? = repository.fetchValue(identifiedBy: child2.metadata.uniqueIdentifier)
+            XCTAssertNotNil(fetchedChild)
+        }
+        
+        let child3 = Child()
+        parent.children = [child1, child3]
+        repository.commit(&parent)
+        
+        do {
+            let fetchedChild:Child? = repository.fetchValue(identifiedBy: child2.metadata.uniqueIdentifier)
+            XCTAssertNil(fetchedChild)
+        }
+    }
+    
+    func testDeletingParentDeletesChildren() {
+        var parent = Parent()
+        let child1 = Child()
+        let child2 = Child()
+        parent.children = [child1, child2]
+        repository.commit(&parent)
+        
+        repository.delete(&parent)
+        
+        let fetchedChild:Child? = repository.fetchValue(identifiedBy: child1.metadata.uniqueIdentifier)
+        XCTAssertNil(fetchedChild)
+    }
+    
+    func testChangingChildToSameChildDoesNotOrphan() {
+        var parent = Parent()
+        parent.children = [Child()]
+        repository.commit(&parent)
+        
+        var child1 = parent.child
+        child1.age = 13
+        parent.children = [child1]
+        repository.commit(&parent)
+
+        let fetchedChild:Child? = repository.fetchValue(identifiedBy: child1.metadata.uniqueIdentifier)
+        XCTAssertNotNil(fetchedChild)
     }
 }
 
